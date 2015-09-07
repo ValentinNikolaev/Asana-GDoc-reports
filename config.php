f0abee732fd2f59a78e47f5c10195fa57102d6aa
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-07 20:51:20 +0300

    % config path at index.php + debug level : 1 + asana client creation

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
 <?php
 
+define('DEBUG', 1);
 define('ASANA_API_KEY', '3ivyLe5P.NK19uB8GNejRbxSigjLSW09');
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-require __DIR__. 'config.php';
+
+require __DIR__. '/config.php';
 require __DIR__ . '/vendor/autoload.php';
+
+switch (DEBUG) {
+    case 1:
+        error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
+        ini_set('display_errors', true);
+        break;
+}
+
+
+/**
+ * Create a client using Asana API key
+ */
+
+$client = Asana\Client::basicAuth(ASANA_API_KEY);
+
+//var_dump($client);
