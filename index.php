b74a2d6e89b883792f29ba2fa68d00ac72b8498f
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-07 21:14:23 +0300

    + find available workspaces

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require __DIR__. '/config.php';
 require __DIR__ . '/vendor/autoload.php';
 
+/**
+ * set debug level
+ */
 switch (DEBUG) {
     case 1:
         error_reporting(E_ALL & ~E_STRICT & ~E_DEPRECATED);
@@ -10,11 +13,18 @@ switch (DEBUG) {
         break;
 }
 
-
 /**
  * Create a client using Asana API key
  */
 
 $client = Asana\Client::basicAuth(ASANA_API_KEY);
 
-//var_dump($client);
+/**
+ * Get all workspaces
+ */
+$currentWorkspaces = [];
+$workspaces = $client->projects->findAll();
+
+if (!$workspaces) {
+    throw new Exception("No workspaces were found");
+}
