19a8e03b9fe963bbccb3c29c04fe54833efffc4d
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-08 19:00:19 +0300

    [google-cli] % dateReport format; + insertFile to google doc

--- google-cli.php
+++ google-cli.php
@@ -220,7 +220,7 @@ function generateXlsReports($data, $fileName) {
 
     $tableStartCell = '';
     $tableCells = [];
-    $dateReport = date('d m Y');
+    $dateReport = date('d.m.Y');
 
     /** try to find meta tags */
     for ($row = 1; $row <= $highestRow; $row++) {
@@ -299,6 +299,7 @@ function generateXlsReports($data, $fileName) {
     $fileReport = REPORTS_PATH. $data['project']->name." ".$dateReport.".xls";
     $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
     $objWriter->save($fileReport);
+    return $fileReport;
 
 }
 
@@ -316,6 +317,51 @@ function getStartTasksDate() {
     $datetime = new DateTime(date('Y-m-d '.TIME_CHECK_FROM, strtotime($previous.' day')));
     return $datetime->format('Y-m-d\TH:i:s\Z');
 
+}
+
+
+/**
+ * Insert new file.
+ *
+ * @param Google_Service_Drive $service Drive API service instance.
+ * @param string $title Title of the file to insert, including the extension.
+ * @param string $description Description of the file to insert.
+ * @param string $parentId Parent folder's ID.
+ * @param string $mimeType MIME type of the file to insert.
+ * @param string $filename Filename of the file to insert.
+ * @return Google_Service_Drive_DriveFile The file that was inserted. NULL is
+ *     returned if an API error occurred.
+ */
+function insertFile($service, $title, $description, $parentId, $mimeType, $filename) {
+    $file = new Google_Service_Drive_DriveFile();
+    $file->setTitle($title);
+    $file->setDescription($description);
+    $file->setMimeType($mimeType);
+
+    // Set the parent folder.
+    if ($parentId != null) {
+        $parent = new Google_Service_Drive_ParentReference();
+        $parent->setId($parentId);
+        $file->setParents(array($parent));
+    }
+
+    try {
+        $data = file_get_contents($filename);
+
+        $createdFile = $service->files->insert($file, array(
+            'data' => $data,
+            'mimeType' => $mimeType,
+            'uploadType' => 'media',
+            'convert' => true,
+        ));
+
+        // Uncomment the following line to print the File ID
+        // print 'File ID: %s' % $createdFile->getId();
+
+        return $createdFile;
+    } catch (Exception $e) {
+        print "An error occurred: " . $e->getMessage();
+    }
 }
 
 function getAsanaTasks($startTasksDate = 'now') {
@@ -461,7 +507,11 @@ if ($templates) {
             printf("Processing Report template %s \n", $template);
             foreach ($tasks['data'] as $taskData) {
 //                var_dump($taskData);die;
-                generateXlsReports($taskData, $template);
+                $fileReport = generateXlsReports($taskData, $template);
+                if (file_exists($fileReport)) {
+                    printf("Uploading report '".basename($fileReport)."' to google drive....\n");
+                    insertFile($service, basename($fileReport), '', null, SHEET_INDEX, $fileReport);
+                }
             }
         }
     }
