843b58242cf165cbb2afefb879ead0108cd840c9
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-10 00:04:43 +0300

    [google-cli] % DATETIME_TIMEZONE_ASANA to HST

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DEBUG', 1);
 define('ASANA_API_KEY', '3ivyLe5P.NK19uB8GNejRbxSigjLSW09');
 define('TIME_CHECK_FROM', '18:45');
 define('DATETIME_TIMEZONE_CURRENT', 'Europe/Kiev');
-define('DATETIME_TIMEZONE_ASANA', 'UTC');
+define('DATETIME_TIMEZONE_ASANA', 'HST');
 
 define('DAILY_REPORT_TEMPLATE', '17mpulibwqYKlWLmBZx_sM6_nEHWfprrsgVCTNSQ6Gfk');
 define('SHEET_INDEX', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
