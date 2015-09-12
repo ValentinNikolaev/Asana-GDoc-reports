d93a159347fdf0fa0a51463832737f2dbe46eb3e
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-12 23:53:23 +0300

    [google-web] +listFolders; + retrieveAllFiles

--- google-web.php
+++ google-web.php
@@ -63,7 +63,9 @@ require __DIR__ . '/config.php';
          */
         function listFiles() {
             var request = gapi.client.drive.files.list({
-                'maxResults': 10
+                'q': '(title = "<?=GDOC_REPORT_DIR_NAME;?>" or title contains "<?=date(DATE_FORMAT_FNAME);?>")' +
+                ' and trashed = false and (mimeType="<?=GDOC_SHEET_MIME;?>" or mimeType="<?=GDOC_FOLDER_MIME;?>")'
+//                'maxResults': 10
             });
 
             request.execute(function(resp) {
@@ -78,6 +80,54 @@ require __DIR__ . '/config.php';
                     appendPre('No files found.');
                 }
             });
+        }
+
+
+        /**
+        * Print folders.
+        */
+        function listFolders() {
+            var request = gapi.client.drive.files.list({
+                'q': 'title = "<?=GDOC_REPORT_DIR_NAME;?>" and trashed = false and mimeType="application/vnd.google-apps.folder"'
+//                'maxResults': 10
+            });
+
+            request.execute(function(resp) {
+                appendPre('Files:');
+                var files = resp.items;
+                if (files && files.length > 0) {
+                    for (var i = 0; i < files.length; i++) {
+                        var file = files[i];
+                        appendPre(file.title + ' (' + file.id + ')');
+                    }
+                } else {
+                    appendPre('No files found.');
+                }
+            });
+        }
+
+        /**
+         * Retrieve a list of File resources.
+         *
+         * @param {Function} callback Function to call when the request is complete.
+         */
+        function retrieveAllFiles(callback) {
+            var retrievePageOfFiles = function(request, result) {
+                request.execute(function(resp) {
+                    result = result.concat(resp.items);
+                    var nextPageToken = resp.nextPageToken;
+                    if (nextPageToken) {
+                        request = gapi.client.drive.files.list({
+                            'pageToken': nextPageToken
+                        });
+                        retrievePageOfFiles(request, result);
+                    } else {
+                        callback(result);
+                    }
+                });
+            }
+            var initialRequest = gapi.client.drive.files.list();
+            retrievePageOfFiles(initialRequest, []);
         }
 
         /**
