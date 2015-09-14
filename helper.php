0b196033fa3abe4e7a70b57ec4e3cd27ac92235b
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-15 00:50:02 +0300

    [helper] - colorizeCli

--- helper.php
+++ helper.php
@@ -13,3 +13,32 @@ function expandHomeDirectory($path)
     }
     return str_replace('~', realpath($homeDirectory), $path);
 }
+
+/**
+ * Colorize Console text
+ * @param $text
+ * @param $status
+ * @return string
+ * @throws Exception
+ */
+function colorizeCli($text, $status)
+{
+    $out = "";
+    switch ($status) {
+        case "SUCCESS":
+            $out = "[42m"; //Green background
+            break;
+        case "FAILURE":
+            $out = "[41m"; //Red background
+            break;
+        case "WARNING":
+            $out = "[43m"; //Yellow background
+            break;
+        case "NOTE":
+            $out = "[44m"; //Blue background
+            break;
+        default:
+            throw new Exception("Invalid status: " . $status);
+    }
+    return chr(27) . "$out" . "$text" . chr(27) . "[0m";
+}
