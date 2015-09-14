d87c1b539f614e724b33b229e1073e881b068708
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-15 00:58:42 +0300

    [bootstrap] + debug support

--- bootstrap.php
+++ bootstrap.php
@@ -3,6 +3,8 @@
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(-1);
+if (DEBUG) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(-1);
+}
