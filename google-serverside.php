d861b55d3ac4774d8b415b1f32cdd7015ad35a33
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-15 00:41:07 +0300

    [google-serverside] % move few code to bootstrap; reformat code

--- google-serverside.php
+++ google-serverside.php
@@ -1,9 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(-1);
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/config.php';
+
+require __DIR__ . '/bootstrap.php';
+require __DIR__ . '/helper.php';
 
 global $client, $credentialsPath;
 
