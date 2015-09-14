f852c5307486c39e0f6f6dd6e847d351ac88f8f5
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-15 00:47:20 +0300

    [google-cli] % move few code to bootstrap

--- google-cli.php
+++ google-cli.php
@@ -1,7 +1,7 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/config.php';
+require __DIR__ . '/bootstrap.php';
+require __DIR__ . '/helper.php';
 //require __DIR__ . '/asana.php';
 
 global $gProjectDir;
