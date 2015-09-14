ca0b847e406625a9dd37865c46e2ea8a72bc45af
Valentin Nikolaev <valenti.nikolaev@gmail.com>
2015-09-15 01:16:43 +0300

    [google-cli] %rename inserFolder to insertFolder

--- google-cli.php
+++ google-cli.php
@@ -313,7 +313,7 @@ function createProjectReportDir($projectName = '')
  * @return Google_Service_Drive_DriveFile The file that was inserted. NULL is
  *     returned if an API error occurred.
  */
-function inserFolder($service, $title, $parentId = 'root')
+function insertFolder($service, $title, $parentId = 'root')
 {
     $dir = new Google_Service_Drive_DriveFile();
     $dir->setTitle($title);
@@ -574,7 +574,7 @@ if ($templates) {
 
     if (!$gProjectDir) {
         print "Create GDrive folder '" . GDOC_REPORT_DIR_NAME . "' \n";
-        $gProjectDir = inserFolder($service, GDOC_REPORT_DIR_NAME);
+        $gProjectDir = insertFolder($service, GDOC_REPORT_DIR_NAME);
     }
 
 
@@ -606,7 +606,7 @@ if ($templates) {
 
                         if (!$foundProjectGDir) {
                             print "Create GDrive folder for project '" . $taskData['project']->name . "' \n";
-                            $saveDir = inserFolder($service, $taskData['project']->name, $gProjectDir->getId());
+                            $saveDir = insertFolder($service, $taskData['project']->name, $gProjectDir->getId());
                         }
 
                         insertFile($service, basename($fileReport), '', $saveDir->getId(), GDOC_SHEET_MIME, $fileReport);
